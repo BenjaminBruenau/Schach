61cde1ac9018ba85b79bdc377b0ed965880d0e75
Benni <benjaminbruenau@gmail.com>
2022-03-29 22:47:03 +0200

    Adjusted Main

--- src/main/scala/Schach/Schach.scala
+++ src/main/scala/Schach/Schach.scala
@@ -4,35 +4,33 @@ import com.google.inject.Guice
 import controller.controllerComponent._
 import aview._
 
-object Schach {
 
 
-  def main(args: Array[String]) = {
+@main def main(): Unit = {
 
-    var break = false
-    val injector = Guice.createInjector(new GameFieldModule)
-    val controller = injector.getInstance(classOf[ControllerInterface])
-    controller.createGameField()
-    val gui = Gui(controller)
-    val tui = Tui(controller)
+  var break = false
+  val injector = Guice.createInjector(new GameFieldModule)
+  val controller = injector.getInstance(classOf[ControllerInterface])
+  controller.createGameField()
+  val gui = Gui(controller)
+  val tui = Tui(controller)
 
-    println("Move the chess pieces: position they are at now -> position they should go to")
-    println("Create a new GameField with 'new'")
-    println("Usage example: move A2 A3")
-    println("Type 'exit' to leave\n")
+  println("Move the chess pieces: position they are at now -> position they should go to")
+  println("Create a new GameField with 'new'")
+  println("Usage example: move A2 A3")
+  println("Type 'exit' to leave\n")
 
-    gui.update()
-    tui.update()
+  gui.update()
+  tui.update()
 
-    while (!break) {
-      val line = scala.io.StdIn.readLine()
+  while (!break) {
+    val line = scala.io.StdIn.readLine()
 
-      if (line.equals("exit")) {
-        break = true
-        println("exiting...")
-      } else {
-        tui.interactWithUser(line)
-      }
+    if (line.equals("exit")) {
+      break = true
+      println("exiting...")
+    } else {
+      tui.interactWithUser(line)
     }
   }
 }
